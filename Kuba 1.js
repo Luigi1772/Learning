549d647a7cdf8e6909234e22cfd45eaac8af6f62
Luigi1772 <157828436+Luigi1772@users.noreply.github.com>
2024-01-26 17:53:51 +0100

    First commit

---
+++ Kuba 1.js
@@ -0,0 +1,53 @@
+// console.log("kuku")
+// const kuku = "kuku";
+// var ole = 2;
+// console.log( kuku + " " + ole)
+
+// if(ole == 1)
+// { 
+//     console.log("ole jest równe jeden")
+// }
+
+// else
+// {
+//     console.log("ole jest różne od jeden") 
+// }
+
+const a = 1
+const b = 2
+const z = 3
+const calculation = "dzielenie";
+
+if(calculation == "dodwanie")
+    console.log ( a + b + z )
+if(calculation == "odejmowanie")
+    console.log ( a - b - z )
+if(calculation == "mnożenie")
+    console.log ( a * b * z )
+if(calculation == "dzielenie")
+    console.log ( a / b / z )
+
+const animals = ["kot", "pies", "ptak"];
+console.log(animals[2] + " " + animals [1]);
+console.log(animals.length)
+
+for(i = 0; i < animals.length; i++)
+{
+    console.log(animals[i]);
+}
+
+var day;
+switch (new Date().getDay()) 
+{
+    case 0:
+      day = "Monday";
+      break;
+    case 1 :
+        day = "Thusday";
+        break;
+    case 5 :
+        day = "Friday";
+        break;
+}  
+
+console.log(day);
